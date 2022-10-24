afc23e59533e49dbe9a7f756c134834b14911ce2
Diana Bouquet <bouquetdiana@gmail.com>
2022-10-24 09:19:11 -0300

    etiqueta analitycs y script

---
+++ script/footer.js
@@ -0,0 +1,3 @@
+document.getElementById("idfooter").innerHTML=`
+<h3>¡¡¡ GRACIAS POR TU VISITA, ESPERAMOS VERTE NUEVAMENTE POR ACÁ !!!</h3>
+<p></p>`
---
+++ script/header.js
@@ -0,0 +1,14 @@
+document.getElementById("idheader").innerHTML=`
+<nav class="container">
+        <a href="index.html"
+          ><img src="imágenes/logo MP2.png" alt="Logo DMP" width="100"
+        /></a>
+        <a href="higiene_gatos.html">HIGIENE GATOS</a>
+        <a href="higiene_perros.html">HIGIENE PERROS</a>
+        <a href="huesos_golosinas.html">VARIEDAD DE HUESOS Y GOLOSINAS</a>
+        <a href="contacto.html">CONTACTO</a>
+      </nav>
+      <div class="titulo">
+        <h1>DISTRIBUIDORA MASCOTAS PUEYRREDÓN</h1>
+        <h3>Distribuidores directos de piedras sanitarias</h3>
+      </div>`
