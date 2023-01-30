d6d533dd993ff7c0835b8bff7032db3d4500038f
Diana Bouquet <bouquetdiana@gmail.com>
2023-01-30 12:27:18 -0300

    color fondo

--- script/footer.js
+++ script/footer.js
@@ -1,3 +1,4 @@
 document.getElementById("idfooter").innerHTML=`
-<h3>¡¡¡ GRACIAS POR TU VISITA, ESPERAMOS VERTE PRONTO POR ACÁ !!!</h3>
+<h3>Gracias por tu visita</h3>
+<h3>Esperamos verte pronto por acá</h3>
 <p></p>`
